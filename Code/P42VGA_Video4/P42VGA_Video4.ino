2d77df9364013692b42cd59ff7e09936439dd764
Claudiu Zissulescu <claziss@synopsys.com>
2021-12-07 21:11:10 +0200

    refurb: Cleanup 0

--- Code/P42VGA_Video4/P42VGA_Video4.ino
+++ Code/P42VGA_Video4/P42VGA_Video4.ino
@@ -8,7 +8,7 @@
 
 
 // supported resolutions:
-//  NTSC 320x200x8bit 
+//  NTSC 320x200x8bit
 //  PAL  300x240x8bit
 
 #include <Arduino.h>
@@ -17,7 +17,7 @@
 // *** Set Video mode and Uno/Mega platform in this header file ***
 #include "VS23S040D.h"
 
-P42Display P42Display; 
+P42Display P42Display;
 
 void setup() {
 	u_int16 P42_Display_ID = 0;
@@ -27,16 +27,16 @@ void setup() {
 	Serial.begin(115200);
 	Serial.println("");
 	Serial.println(F("P42 VGA Shield Test - Composite Output"));
-#ifdef NTSC320x200 
+#ifdef NTSC320x200
 		Serial.println(F("Resolution: NTSC 320x200 8bit"));
 #endif
-#ifdef NTSC426x200 
+#ifdef NTSC426x200
 		Serial.println(F("Resolution: NTSC 426x200 8bit"));
 #endif
-#ifdef PAL300x240 
+#ifdef PAL300x240
 		Serial.println(F("Resolution: PAL 300x240 8bit"));
 #endif
-#ifdef PAL500x240 
+#ifdef PAL500x240
 		Serial.println(F("Resolution: PAL 500x240 8bit"));
 #endif
 
@@ -51,7 +51,7 @@ void setup() {
 	pinMode(11, INPUT);
 	pinMode(12, INPUT);
 	pinMode(13, INPUT);
-#endif	
+#endif
 
 	// Config SPI interface
 	pinMode(slaveSelectPin, OUTPUT);
@@ -61,8 +61,8 @@ void setup() {
 	SPI.begin();
 	SPI.setClockDivider(SPI_CLOCK_DIV2);
 	SPI.setDataMode(SPI_MODE0);
-	SPI.setBitOrder(MSBFIRST) ; 
-	
+	SPI.setBitOrder(MSBFIRST) ;
+
 	P42_Display_ID = P42Display.Config( CH0 );
 	P42_Display_ID = P42Display.Config( CH1 );
 	P42_Display_ID = P42Display.Config( CH2 );
@@ -78,7 +78,7 @@ void Mandelbrot (byte channel, float Xn, float Xp, float Yn, float Yp){
 	u_int16 Px, Py;
 	u_int16 iteration = 0;
 	u_int16 max_iteration = 256;
-	
+
 	for (Py = 0; Py < YPIXELS; Py++){
 		y0 = (Yp - Yn)/YPIXELS*Py + Yn;
 		for (Px = 0; Px < XPIXELS; Px++){
@@ -101,15 +101,13 @@ void Mandelbrot (byte channel, float Xn, float Xp, float Yn, float Yp){
 
 void loop() {
 
-	word adc_value = 0;
 	byte incomingByte = 0;   // for incoming serial data
-	byte GPIOControlReg = 0;
 	u_int32 i,j,x,y; // Used for counters
 	u_int32 address;
 	byte value;
-	
+
 	Serial.println(F("Image address map "));
-	
+
 	address = PICLINE_BYTE_ADDRESS(0) + 0;
 	Serial.print(F("Address pixel (0,0): 0x"));
 	Serial.println(address, HEX );
@@ -132,8 +130,8 @@ void loop() {
 	Serial.print(YPIXELS);
 	Serial.print(F("): 0x"));
 	Serial.println(address, HEX );
-	
-	// Test GPIO 
+
+	// Test GPIO
 	Serial.println(F("Test GPIO#4 - set high during clear screen operation") );
 	P42Display.SPIWriteRegister( WriteGPIOControl, PIO4Dir | PIO4High, false  );
 	P42Display.ClearScreen ( CH0, 0x41 );
@@ -142,7 +140,7 @@ void loop() {
 	P42Display.ClearScreen ( CH3, 0x00 );
 	P42Display.SPIWriteRegister( WriteGPIOControl, 0x00, false );
 
-	
+
 	// Draw  colour map test image
 	Serial.println("Draw colour map test image");
 
@@ -165,15 +163,15 @@ void loop() {
 		P42Display.FilledRectangle( CH3, (i*XSIZEREC),              (j*YSIZEREC)+(YSIZEREC-1), (i*XSIZEREC)+(XSIZEREC-1), (j*YSIZEREC)+(YSIZEREC-1), 0);			// Draw black horizontal line
 		P42Display.FilledRectangle( CH3, (i*XSIZEREC)+(XSIZEREC-1), (j*YSIZEREC),              (i*XSIZEREC)+(XSIZEREC-1), (j*YSIZEREC)+(YSIZEREC-1), 0);			// Draw black vertical line
 
-		P42Display.PrintString ( CH0, "Status and Progress on Serial Monitor", 0, 0, 0x13);
-		P42Display.PrintString ( CH0, "115200 8N1.", 0, 8, 0x13);
+		P42Display.PrintString ( CH0, (char *) "Status and Progress on Serial Monitor", 0, 0, 0x13);
+		P42Display.PrintString ( CH0, (char *) "115200 8N1.", 0, 8, 0x13);
 
 	}
 
 	Serial.println(F("Display Image [press key]") );
 	while (Serial.available() == 0) {};
 	incomingByte = Serial.read();
-	
+
 	P42Display.DisplayBMPFromFlash ( CH0, 0x3000, 10, 10 );
 	P42Display.DisplayBMPFromFlash ( CH1, 0x3000, 20, 20 );
 	P42Display.DisplayBMPFromFlash ( CH2, 0x3000, 30, 30 );
@@ -187,16 +185,16 @@ void loop() {
 	P42Display.ClearScreen ( CH1, 0x00 );
 	P42Display.ClearScreen ( CH2, 0x00 );
 	P42Display.ClearScreen ( CH3, 0x00 );
-	
+
 	u_int32 start_time = 0;
 	u_int32 current_time = 0;
-	
+
 	start_time = millis();
-	
+
 	Mandelbrot ( CH0, -2.5, 1, -1, 1);
 
 	current_time = millis();
-	
+
 	Serial.print (F("Mandelbrot duration [msec]"));
 	Serial.println ( current_time - start_time );
 
@@ -237,7 +235,7 @@ void loop() {
 			P42Display.SetRGBPixel (CH1,  j,   i, 0x00ffffff);
 			P42Display.SetRGBPixel (CH2,  j,   i, 0x00ffffff);
 			P42Display.SetRGBPixel (CH3,  j,   i, 0x00ffffff);
-	
+
 	// more colour bars
 	Serial.println(F("4 YUV Pixel [press key]") );
 	while (Serial.available() == 0) {};
@@ -275,7 +273,7 @@ void loop() {
 			P42Display.SetYUVPixel (CH1,  j,   i, 0xbf);
 			P42Display.SetYUVPixel (CH2,  j,   i, 0xbf);
 			P42Display.SetYUVPixel (CH3,  j,   i, 0xbf);
-	
+
 	// single character print
 
 	Serial.println(F("Character [press key]"));
@@ -284,12 +282,12 @@ void loop() {
 
 	P42Display.PrintChar ( CH0, '4', 0, 40, 0xEE);
 	P42Display.PrintChar ( CH0, '2', 8, 40, 0xEE);
-	
+
 	P42Display.PrintChar ( CH1, '1', 8, 40, 0xEE);
 	P42Display.PrintChar ( CH2, '2', 8, 40, 0xEE);
 	P42Display.PrintChar ( CH3, '3', 8, 40, 0xEE);
-	
-	
+
+
 	// String output all characters
 	Serial.println(F("All characters [press key]"));
 	while (Serial.available() == 0) {};
@@ -299,22 +297,22 @@ void loop() {
 	P42Display.ClearScreen ( CH1,  0x02 );
 	P42Display.ClearScreen ( CH2,  0x02 );
 	P42Display.ClearScreen ( CH3,  0x02 );
-	P42Display.PrintString ( CH0, "!\"#$%&\'()*+,-./0123456789:;<=>?@", 0, 0, 0x05);
-	P42Display.PrintString ( CH0, "ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`", 0, 16, 0x05);
-	P42Display.PrintString ( CH0, "abcdefghijklmnopqrstuvwxyz{|}~", 0, 32, 0x05);
-	P42Display.PrintString ( CH0, "CHANNEL 0",  0, 48, 0x05);
-	P42Display.PrintString ( CH1, "!\"#$%&\'()*+,-./0123456789:;<=>?@", 0, 0, 0x05);
-	P42Display.PrintString ( CH1, "ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`", 0, 16, 0x05);
-	P42Display.PrintString ( CH1, "abcdefghijklmnopqrstuvwxyz{|}~", 0, 32, 0x05);
-	P42Display.PrintString ( CH1, "CHANNEL 1", 20, 48, 0x05);
-	P42Display.PrintString ( CH2, "!\"#$%&\'()*+,-./0123456789:;<=>?@", 0, 0, 0x05);
-	P42Display.PrintString ( CH2, "ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`", 0, 16, 0x05);
-	P42Display.PrintString ( CH2, "abcdefghijklmnopqrstuvwxyz{|}~", 0, 32, 0x05);
-	P42Display.PrintString ( CH2, "CHANNEL 2", 40, 48, 0x05);
-	P42Display.PrintString ( CH3, "!\"#$%&\'()*+,-./0123456789:;<=>?@", 0, 0, 0x05);
-	P42Display.PrintString ( CH3, "ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`", 0, 16, 0x05);
-	P42Display.PrintString ( CH3, "abcdefghijklmnopqrstuvwxyz{|}~", 0, 32, 0x05);
-	P42Display.PrintString ( CH3, "CHANNEL 3", 60, 48, 0x05);
+	P42Display.PrintString ( CH0, (char *) "!\"#$%&\'()*+,-./0123456789:;<=>?@", 0, 0, 0x05);
+	P42Display.PrintString ( CH0, (char *) "ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`", 0, 16, 0x05);
+	P42Display.PrintString ( CH0, (char *) "abcdefghijklmnopqrstuvwxyz{|}~", 0, 32, 0x05);
+	P42Display.PrintString ( CH0, (char *) "CHANNEL 0",  0, 48, 0x05);
+	P42Display.PrintString ( CH1, (char *) "!\"#$%&\'()*+,-./0123456789:;<=>?@", 0, 0, 0x05);
+	P42Display.PrintString ( CH1, (char *) "ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`", 0, 16, 0x05);
+	P42Display.PrintString ( CH1, (char *) "abcdefghijklmnopqrstuvwxyz{|}~", 0, 32, 0x05);
+	P42Display.PrintString ( CH1, (char *) "CHANNEL 1", 20, 48, 0x05);
+	P42Display.PrintString ( CH2, (char *) "!\"#$%&\'()*+,-./0123456789:;<=>?@", 0, 0, 0x05);
+	P42Display.PrintString ( CH2, (char *) "ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`", 0, 16, 0x05);
+	P42Display.PrintString ( CH2, (char *) "abcdefghijklmnopqrstuvwxyz{|}~", 0, 32, 0x05);
+	P42Display.PrintString ( CH2, (char *) "CHANNEL 2", 40, 48, 0x05);
+	P42Display.PrintString ( CH3, (char *) "!\"#$%&\'()*+,-./0123456789:;<=>?@", 0, 0, 0x05);
+	P42Display.PrintString ( CH3, (char *) "ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`", 0, 16, 0x05);
+	P42Display.PrintString ( CH3, (char *) "abcdefghijklmnopqrstuvwxyz{|}~", 0, 32, 0x05);
+	P42Display.PrintString ( CH3, (char *) "CHANNEL 3", 60, 48, 0x05);
 
 	// String output C-64 inspired start screen :-)
 	Serial.println("String [press key]");
@@ -322,24 +320,24 @@ void loop() {
 	incomingByte = Serial.read();
 
 	P42Display.ClearScreen ( CH0, 0x5c );
-	P42Display.PrintString ( CH0, "**** ARDUINO P42 VIDEO4 SHIELD ****", 8, 0, 0x55);
-	P42Display.PrintString ( CH0, "16M FLASH SYSTEM A LOT OF BYTES FREE.", 8, 16, 0x54);
-	P42Display.PrintString ( CH0, "CH0 READY.", 0, 32, 0x53);
+	P42Display.PrintString ( CH0, (char *) "**** ARDUINO P42 VIDEO4 SHIELD ****", 8, 0, 0x55);
+	P42Display.PrintString ( CH0, (char *) "16M FLASH SYSTEM A LOT OF BYTES FREE.", 8, 16, 0x54);
+	P42Display.PrintString ( CH0, (char *) "CH0 READY.", 0, 32, 0x53);
 
 	P42Display.ClearScreen ( CH1, 0x7c );
-	P42Display.PrintString ( CH1, "**** ARDUINO P42 VIDEO4 SHIELD ****", 8, 0, 0x65);
-	P42Display.PrintString ( CH1, "16M FLASH SYSTEM A LOT OF BYTES FREE.", 8, 16, 0x64);
-	P42Display.PrintString ( CH1, "CH1 READY.", 0, 32, 0x63);
+	P42Display.PrintString ( CH1, (char *) "**** ARDUINO P42 VIDEO4 SHIELD ****", 8, 0, 0x65);
+	P42Display.PrintString ( CH1, (char *) "16M FLASH SYSTEM A LOT OF BYTES FREE.", 8, 16, 0x64);
+	P42Display.PrintString ( CH1, (char *) "CH1 READY.", 0, 32, 0x63);
 
 	P42Display.ClearScreen ( CH2, 0x8c );
-	P42Display.PrintString ( CH2, "**** ARDUINO P42 VIDEO4 SHIELD ****", 8, 0, 0x75);
-	P42Display.PrintString ( CH2, "16M FLASH SYSTEM A LOT OF BYTES FREE.", 8, 16, 0x74);
-	P42Display.PrintString ( CH2, "CH2 READY.", 0, 32, 0x73);
+	P42Display.PrintString ( CH2, (char *) "**** ARDUINO P42 VIDEO4 SHIELD ****", 8, 0, 0x75);
+	P42Display.PrintString ( CH2, (char *) "16M FLASH SYSTEM A LOT OF BYTES FREE.", 8, 16, 0x74);
+	P42Display.PrintString ( CH2, (char *) "CH2 READY.", 0, 32, 0x73);
 
 	P42Display.ClearScreen ( CH3, 0xec );
-	P42Display.PrintString ( CH3, "**** ARDUINO P42 VIDEO4 SHIELD ****", 8, 0, 0x85);
-	P42Display.PrintString ( CH3, "16M FLASH SYSTEM A LOT OF BYTES FREE.", 8, 16, 0x84);
-	P42Display.PrintString ( CH3, "CH3 READY.", 0, 32, 0x83);
+	P42Display.PrintString ( CH3, (char *) "**** ARDUINO P42 VIDEO4 SHIELD ****", 8, 0, 0x85);
+	P42Display.PrintString ( CH3, (char *) "16M FLASH SYSTEM A LOT OF BYTES FREE.", 8, 16, 0x84);
+	P42Display.PrintString ( CH3, (char *) "CH3 READY.", 0, 32, 0x83);
 
 	Serial.println(F("Next [press key]") );
 	while (Serial.available() == 0) {
@@ -376,19 +374,19 @@ void loop() {
 	// Amiga Style Boing Ball demo
 	Serial.println(F("BoingBall [press key]") );
 
-	// BoingBall animation 
+	// BoingBall animation
 	P42Display.ClearScreen ( CH0, 0x00 );
 	P42Display.ClearScreen ( CH1, 0x00 );
 	P42Display.ClearScreen ( CH2, 0x00 );
 	P42Display.ClearScreen ( CH3, 0x00 );
-	
+
 	const int     BOINGBALL_ADDR		= 0x1000;
 	const u_int32 START_FREE_VIDEO_MEM	= (PICLINE_BYTE_ADDRESS(YPIXELS-1) + XPIXELS-1) +1;
 	const byte    BALLX 				= 32;
 	const byte    BALLY 				= 32;
 	const int     BOINGBALL_BYTES		= BALLX * BALLY *8;
 	const u_int32 MEMBYTES_LINE 		= (PICLINE_BYTE_ADDRESS(1) + 0) - (PICLINE_BYTE_ADDRESS(0) + 0);
-	
+
 	u_int32 skip = 0;
 
 	// copy ball data into video memory after picture data.
@@ -400,8 +398,8 @@ void loop() {
 		SPI.transfer( byte((BOINGBALL_ADDR+i >> 8) & 0xFF) );
 		SPI.transfer( byte( BOINGBALL_ADDR+i       & 0xFF) );
 			value = SPI.transfer( 0x00 );
-		digitalWrite(MemSelectPin,HIGH); 
-		
+		digitalWrite(MemSelectPin,HIGH);
+
 		// scramble lines in video memory
 		// the block transfer function needs to have the skip area for source and target memory. Really VLSI?
 		if (i% (BALLX*BALLY) == 0)
@@ -410,7 +408,7 @@ void loop() {
 			skip = skip + (MEMBYTES_LINE-BALLX)+1;
 		else
 			skip++;
-				
+
 		P42Display.SPIWriteByte (CH0, START_FREE_VIDEO_MEM + skip , value, false);
 		P42Display.SPIWriteByte (CH1, START_FREE_VIDEO_MEM + skip , value, false);
 		P42Display.SPIWriteByte (CH2, START_FREE_VIDEO_MEM + skip , value, false);
@@ -425,21 +423,21 @@ void loop() {
 	int y_dir = 1;
 	int frame_dir = 1;
 	u_int32 moveblock2 = 0;
-	u_int32 source, target, control;
 
+	randomSeed(analogRead(0));
 	while (Serial.available() == 0) {
-	
+
 		// enable channel 0 for the line counter.
 		P42Display.SPIWriteRegister (WriteMultiICAccessControl, 0xfe, false);
 		// wait for line counter to be outside of visible area
 		while ( P42Display.SPIReadRegister16 ( ReadCurrentLinePLL, false) & 0x01ff < ENDLINE ) {}
-		
+
 		// enable all the channels.
 		P42Display.SPIWriteRegister (WriteMultiICAccessControl, 0x00, false);
 		// move block
-		P42Display.SPIWriteRegister40 (WriteBlockMoveControl1, 
-			((u_int32)(START_FREE_VIDEO_MEM + frame_no*BALLX) >>1 & 0xFFFF), 
-			((PICLINE_BYTE_ADDRESS(y-1) + x-1) >>1 & 0xFFFF), 
+		P42Display.SPIWriteRegister40 (WriteBlockMoveControl1,
+			((u_int32)(START_FREE_VIDEO_MEM + frame_no*BALLX) >>1 & 0xFFFF),
+			((PICLINE_BYTE_ADDRESS(y-1) + x-1) >>1 & 0xFFFF),
 			BMVC_PYF | ((u_int32)(START_FREE_VIDEO_MEM + frame_no*BALLX) & 0x00001) <<2 | ((PICLINE_BYTE_ADDRESS(y-1) + x-1) & 0x00001) <<1 , false);
 		moveblock2 = (u_int32)((u_int32)(MEMBYTES_LINE-BALLX)<<16) | (BALLX<<8) | BALLY;
 		P42Display.SPIWriteRegister32 (WriteBlockMoveControl2, moveblock2, false);
@@ -447,9 +445,8 @@ void loop() {
 
 		x=x+x_dir;
 		y=y+y_dir;
-		
+
 		// set new ball direction and rotation at the borders
-		randomSeed(analogRead(0));
 		if ( x <= 1 ) {
 			x_dir  = 1;
 			y_dir = random(-1, 2);
@@ -461,7 +458,7 @@ void loop() {
 			frame_dir = random(1, 3);
 		}
 
-		if ( y <= 1 ) { 
+		if ( y <= 1 ) {
 			y_dir  = 1;
 			x_dir = random(-1, 2);
 			frame_dir = random(1, 3);
@@ -471,24 +468,24 @@ void loop() {
 			x_dir = random(-1, 2);
 			frame_dir = random(1, 3);
 		}
-		
+
 		//rotation
 		if (frame_dir == 1) {
 			frame_no++;
-			if (frame_no > 7) 
+			if (frame_no > 7)
 				frame_no = 0;
 		}
 		else if (frame_dir == 2) {
-			if (frame_no == 0) 
+			if (frame_no == 0)
 				frame_no = 7;
 			else
 				frame_no--;
 		}
 		else {
 			frame_no++;
-			if (frame_no > 7) 
+			if (frame_no > 7)
 				frame_no = 0;
-		}		
+		}
 		// wait for line counter to be at beginning of visible area
 		//while ( P42Display.SPIReadRegister16 ( ReadCurrentLinePLL, false) & 0x01ff > ENDLINE ) {}
 		delay (20);
--- Code/P42VGA_Video4/VS23S040D.h
+++ Code/P42VGA_Video4/VS23S040D.h
@@ -1,4 +1,4 @@
-// VS23S010D-L header file for 
+// VS23S010D-L header file for
 // P42 Video Display Shield for Arduino
 // Wolfgang Friedrich
 // Last change: 2018/Feb/11
@@ -63,13 +63,13 @@ const int DORead						= 0x3B;		// Dual Output Read
 const int DORead_DualAddress			= 0xBB;
 const int QORead						= 0x6B;		// Quad Output Write
 const int QORead_QuadAddress			= 0xEB;
-// SRAM Write Commands	
+// SRAM Write Commands
 const int WriteSRAM						= 0x02;
 const int DIWrite						= 0xA2;		// Dual Input Read
 const int DIWrite_DualAddress			= 0x22;
 const int QIWrite						= 0x32;		// Quad Input Read
 const int QIWrite_QuadAddress			= 0xB2;
-// Miscellaneous Commands	
+// Miscellaneous Commands
 const int ReadStatus					= 0x05;
 const int WriteStatus					= 0x01;
 const int ReadDeviceID					= 0x9F;
@@ -78,7 +78,7 @@ const int WriteGPIOControl				= 0x82;
 const int ReadGPIOState					= 0x86;
 const int ReadMultiICAccessControl		= 0xB7;
 const int WriteMultiICAccessControl		= 0xB8;
-// Video Display Controller Commands	
+// Video Display Controller Commands
 const int WritePictureStart				= 0x28;
 const int WritePictureEnd				= 0x29;
 const int WriteLineLength				= 0x2A;
@@ -134,36 +134,44 @@ class P42Display
 {
 	public:
 		P42Display();
-		byte SPIReadRegister (byte address, boolean debug);
-		word SPIReadRegister16 (byte address, boolean debug);
-		void SPIWriteRegister (byte address, byte value, boolean debug);
-		void SPIWriteRegister16 (byte address, word value, boolean debug );
-		void SPIWriteRegister32 (byte address, u_int32 value, boolean debug );
-		void SPIWriteRegister40 (byte address, word source, word target, byte control, boolean debug );
-		word Config( byte channel );
-//		byte SPITransferByte (u_int32 address, byte value, boolean debug);
-//		word SPITransferWord (u_int32 address, byte value, boolean debug);
-		byte SPIReadByte (byte channel, u_int32 address);
-		word SPIReadWord (byte channel, u_int32 address, boolean debug);
-		void SPIWriteByte (byte channel, u_int32 address, byte value, boolean debug );
-		void SPIWriteWord (byte channel, u_int32 address, word value, boolean debug );
+		uint8_t SPIReadRegister (uint8_t address, boolean debug);
+		uint16_t SPIReadRegister16 (uint8_t address, boolean debug);
+		void SPIWriteRegister (uint8_t address, uint8_t value, boolean debug);
+		void SPIWriteRegister16 (uint8_t address, uint16_t value, boolean debug );
+		void SPIWriteRegister32 (uint8_t address, uint32_t value, boolean debug );
+		void SPIWriteRegister40 (uint8_t address, uint16_t source, uint16_t target, uint8_t control, boolean debug );
+		uint16_t Config( uint8_t channel );
+//		uint8_t SPITransferUint8_T (uint32_t address, uint8_t value, boolean debug);
+//		uint16_t SPITransferWord (uint32_t address, uint8_t value, boolean debug);
+		uint8_t SPIReadByte (uint8_t channel, uint32_t address);
+		uint16_t SPIReadWord (uint8_t channel, uint32_t address, boolean debug);
+		void SPIWriteByte (uint8_t channel, uint32_t address, uint8_t value, boolean debug );
+		void SPIWriteWord (uint8_t channel, uint32_t address, uint16_t value, boolean debug );
 		// Graphics commands
-		void FilledRectangle (byte channel, u_int16 x1, u_int16 y1, u_int16 x2, u_int16 y2, u_int16 color);
-		void ClearScreen (byte channel,  byte colour );
-		void SetRGBPixel (byte channel, word x, word y, u_int32 colour);	// colour is 0x00rrggbb
-		void SetYUVPixel (byte channel, word x, word y, byte colour);   			// colour is single word
-		void PrintChar (byte channel, char Letter, word x, word y, byte colour);	// YUV colour
-		void PrintString (byte channel, char* Text, word x, word y, byte colour);	// YUV colour
-		// block move 
-		
+		void FilledRectangle (uint8_t channel, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t color);
+		void ClearScreen (uint8_t channel,  uint8_t colour );
+		void SetRGBPixel (uint8_t channel, uint16_t x, uint16_t y, uint32_t colour);	// colour is 0x00rrggbb
+		void SetYUVPixel (uint8_t channel, uint16_t x, uint16_t y, uint8_t colour);   			// colour is single word
+		void PrintChar (uint8_t channel, char Letter, uint16_t x, uint16_t y, uint8_t colour);	// YUV colour
+		void PrintString (uint8_t channel, char* Text, uint16_t x, uint16_t y, uint8_t colour);	// YUV colour
+		// block move
+
 		// Data copy
-		byte UARTDataToFlash ( u_int32 length, u_int32 mem_location);   // receive number of bytes on UART and write to Flash memory address
-		void DisplayBMPFromFlash (byte channel, u_int32 mem_location, u_int16 x, u_int16 y );	// 
- 	private:
-		void _protoline(byte channel, u_int16 line, u_int16 offset, u_int16 limit, u_int16 data);
-		void _printdebug (byte address, u_int32 value);
-	
+		uint8_t UARTDataToFlash ( uint32_t length, uint32_t mem_location);   // receive number of bytes on UART and write to Flash memory address
+		void DisplayBMPFromFlash (uint8_t channel, uint32_t mem_location, uint16_t x, uint16_t y );	//
+	private:
+		void _protoline(uint8_t channel, uint16_t line, uint16_t offset, uint16_t limit, uint16_t data);
+		void _printdebug (uint8_t address, uint32_t value);
+
 };
+
+#ifndef ATTRIBUTE_UNUSED
+#if GCC_VERSION >= 3004
+#  define ATTRIBUTE_UNUSED __attribute__ ((__unused__))
+#else
+#define ATTRIBUTE_UNUSED
+#endif
+#endif /* ATTRIBUTE_UNUSED */
 
 
 #endif
